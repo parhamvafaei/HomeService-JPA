784c517cc387df2136ae43f1a70498c6944cd1be
parham <p.vafaeei@gmail.com>
2022-12-06 19:57:42 +0330

    create BaseEntity and make superClass

---
+++ src/main/java/org/maktab/base/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package org.maktab.base.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDate createTime;
+
+    @UpdateTimestamp
+    private   LocalDate updateTime;
+}
