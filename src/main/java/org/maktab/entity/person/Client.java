c500bd01519647150134de819a72cac638f3bade
parham <p.vafaeei@gmail.com>
2022-12-09 08:36:11 +0330

    create Client class

---
+++ src/main/java/org/maktab/entity/person/Client.java
@@ -0,0 +1,28 @@
+package org.maktab.entity.person;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+import org.maktab.entity.Credit;
+import org.maktab.entity.Order;
+import org.maktab.entity.SubService;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Client extends Person {
+
+    @OneToOne
+private Credit credit;
+
+    @OneToMany(mappedBy = "client" ,cascade = CascadeType.ALL)
+    private List<Order> orders;
+}
