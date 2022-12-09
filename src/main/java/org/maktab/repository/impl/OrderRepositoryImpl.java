de08834ec7499131790b6b0e9c82819cc2b49e25
parham <p.vafaeei@gmail.com>
2022-12-09 09:19:36 +0330

    Create OrderRepositoryImpl class

---
+++ src/main/java/org/maktab/repository/impl/OrderRepositoryImpl.java
@@ -0,0 +1,8 @@
+package org.maktab.repository.impl;
+
+import org.maktab.base.repository.impl.BaseRepositoryImpl;
+import org.maktab.entity.Order;
+import org.maktab.repository.OrderRepository;
+
+public class OrderRepositoryImpl extends BaseRepositoryImpl<Order> implements OrderRepository {
+}
