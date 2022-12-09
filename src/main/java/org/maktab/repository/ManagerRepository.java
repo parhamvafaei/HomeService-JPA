a3d7fccfef63e797a2157f80a9d67bbfd9a9e64a
parham <p.vafaeei@gmail.com>
2022-12-09 09:40:36 +0330

    create ManagerRepository interface

---
+++ src/main/java/org/maktab/repository/ManagerRepository.java
@@ -0,0 +1,7 @@
+package org.maktab.repository;
+
+import org.maktab.base.repository.BaseRepository;
+import org.maktab.entity.person.Manager;
+
+public interface ManagerRepository extends BaseRepository<Manager> {
+}
