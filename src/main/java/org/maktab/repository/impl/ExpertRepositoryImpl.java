714fb21d9fcf8fdb5bdb0619bbd6d205dc97a241
parham <p.vafaeei@gmail.com>
2022-12-09 09:22:27 +0330

    create ExpertRepositoryImpl class

---
+++ src/main/java/org/maktab/repository/impl/ExpertRepositoryImpl.java
@@ -0,0 +1,8 @@
+package org.maktab.repository.impl;
+
+import org.maktab.base.repository.impl.BaseRepositoryImpl;
+import org.maktab.entity.person.Expert;
+import org.maktab.repository.ExpertRepository;
+
+public class ExpertRepositoryImpl extends BaseRepositoryImpl<Expert> implements ExpertRepository {
+}
