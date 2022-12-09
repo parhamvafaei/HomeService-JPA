ba592f7214edb4b02f5d153949e104a4deadf775
parham <p.vafaeei@gmail.com>
2022-12-09 09:18:37 +0330

    Create OrderRepository interface

---
+++ src/main/java/org/maktab/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package org.maktab.repository;
+
+import org.maktab.base.entity.BaseEntity;
+import org.maktab.base.repository.BaseRepository;
+import org.maktab.entity.Order;
+
+public interface OrderRepository extends BaseRepository<Order> {
+}
