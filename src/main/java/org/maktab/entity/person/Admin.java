fc99b7dbdd7c88bc02811b0c8980d9e1f8455472
parham <p.vafaeei@gmail.com>
2022-12-09 08:35:46 +0330

    create admin class

---
+++ src/main/java/org/maktab/entity/person/Admin.java
@@ -0,0 +1,15 @@
+package org.maktab.entity.person;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Admin extends Person {
+
+
+}
