c59281b8bd775df581d9c5730a2714ec9ed441ff
parham <p.vafaeei@gmail.com>
2022-12-09 09:08:35 +0330

    create AdminRepositoryImpl class

---
+++ src/main/java/org/maktab/repository/impl/AdminRepositoryImpl.java
@@ -0,0 +1,18 @@
+package org.maktab.repository.impl;
+
+import jakarta.persistence.EntityManager;
+import org.maktab.base.repository.impl.BaseRepositoryImpl;
+import org.maktab.entity.person.Admin;
+import org.maktab.repository.AdminRepository;
+
+public class AdminRepositoryImpl extends BaseRepositoryImpl<Admin> implements AdminRepository {
+
+    public AdminRepositoryImpl(EntityManager em) {
+        super(em);
+    }
+
+    @Override
+    public Class<Admin> getEntityClass() {
+        return Admin.class;
+    }
+}
