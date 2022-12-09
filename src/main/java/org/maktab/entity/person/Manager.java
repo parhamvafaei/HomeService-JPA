d019ba522b26fe2f54c217e216a865cee754b983
parham <p.vafaeei@gmail.com>
2022-12-09 08:37:28 +0330

    create Manager class

---
+++ src/main/java/org/maktab/entity/person/Manager.java
@@ -0,0 +1,14 @@
+package org.maktab.entity.person;
+
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Manager extends Person {
+
+}
