ee4f2deb4c3d7d067f4ec5aa3cf87905bc457126
parham <p.vafaeei@gmail.com>
2022-12-09 09:16:19 +0330

    Crete OfferRepository interface

---
+++ src/main/java/org/maktab/repository/OfferRepository.java
@@ -0,0 +1,7 @@
+package org.maktab.repository;
+
+import org.maktab.base.repository.BaseRepository;
+import org.maktab.entity.Offer;
+
+public interface OfferRepository extends BaseRepository<Offer> {
+}
