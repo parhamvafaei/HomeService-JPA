be4eeb311cf64eb204ca8de97596fdab07299110
parham <p.vafaeei@gmail.com>
2022-12-09 08:42:10 +0330

    create ExpertStatus enum class

--- src/main/java/org/maktab/Main.java
+++ src/main/java/org/maktab/Main.java
@@ -1,11 +1,33 @@
 package org.maktab;
 
 
+
+import jakarta.persistence.EntityManager;
+import org.maktab.entity.Address;
+import org.maktab.exceptionhandler.RepetitiveServiceException;
 import org.maktab.util.JpaConnection;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        JpaConnection.getEntityManagerFactory().createEntityManager();
-    }
+//        EntityManager entityManager = JpaConnection.getEntityManagerFactory().createEntityManager();
+//        entityManager.getTransaction().begin();
+//entityManager.persist(new Address());
+//        entityManager.getTransaction().commit();
+        //throw new RepetitiveServiceException();
 
-}
+//        Service[] services = Service.getServices();
+//        for (int i = 0; i < services.length; i++) {
+//            System.out.println(services[i].toString());
+
+//        Duration timeDuration=Duration.ofHours(2);
+//        timeDuration.plusHours(10);
+//        System.out.println(timeDuration.toHours());
+
+
+        //create new entity to set orders and define expert order offer subService
+
+        }
+    }
