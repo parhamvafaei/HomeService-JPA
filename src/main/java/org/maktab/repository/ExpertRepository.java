28e3ef1d75ea22089de8cea2cabf778f83d2cea3
parham <p.vafaeei@gmail.com>
2022-12-09 09:22:08 +0330

    create ExpertRepository interface

---
+++ src/main/java/org/maktab/repository/ExpertRepository.java
@@ -0,0 +1,7 @@
+package org.maktab.repository;
+
+import org.maktab.base.repository.BaseRepository;
+import org.maktab.entity.person.Expert;
+
+public interface ExpertRepository extends BaseRepository<Expert> {
+}
