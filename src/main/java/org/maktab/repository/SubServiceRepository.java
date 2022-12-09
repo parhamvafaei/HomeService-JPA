ccff381d3e89d6cfe7824bf1a0aaac492410d32f
parham <p.vafaeei@gmail.com>
2022-12-09 09:21:14 +0330

    create SubServiceRepository interface

---
+++ src/main/java/org/maktab/repository/SubServiceRepository.java
@@ -0,0 +1,7 @@
+package org.maktab.repository;
+
+import org.maktab.base.repository.BaseRepository;
+import org.maktab.entity.SubService;
+
+public interface SubServiceRepository extends BaseRepository<SubService> {
+}
