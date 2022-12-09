654b75f651fff4253d0ea6741805f98b30f9ff86
parham <p.vafaeei@gmail.com>
2022-12-09 08:37:12 +0330

    create Expert class

---
+++ src/main/java/org/maktab/entity/person/Expert.java
@@ -0,0 +1,49 @@
+package org.maktab.entity.person;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.maktab.entity.Credit;
+import org.maktab.entity.Offer;
+import org.maktab.entity.Order;
+import org.maktab.entity.SubService;
+
+
+import javax.validation.constraints.Size;
+import java.util.List;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Expert extends Person {
+
+    private Float rating = 0.0F;
+
+    @Lob
+    @Size(max = 300_000)
+    private byte[] photo;
+
+    @Enumerated
+    private ExpertStatus expertStatus;
+
+    @ManyToMany
+    @JoinTable(name = "expert_subservice",
+            joinColumns = @JoinColumn(name = "expert_id"),
+            inverseJoinColumns = @JoinColumn(name = "subservice_id"))
+    @ToString.Exclude
+    private List<SubService> subServices;
+
+    @OneToMany(mappedBy = "expert" , cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<Offer> offers;
+
+//    @OneToMany
+//    @ToString.Exclude
+//    private List<Order> orders;
+
+    @OneToOne
+    private Credit credit;
+}
