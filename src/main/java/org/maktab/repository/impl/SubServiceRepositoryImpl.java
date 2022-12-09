2829e93ceb95af2aa849787d60a39f7993cbd877
parham <p.vafaeei@gmail.com>
2022-12-09 09:21:31 +0330

    create SubServiceRepositoryImpl class

---
+++ src/main/java/org/maktab/repository/impl/SubServiceRepositoryImpl.java
@@ -0,0 +1,8 @@
+package org.maktab.repository.impl;
+
+import org.maktab.base.repository.impl.BaseRepositoryImpl;
+import org.maktab.entity.SubService;
+import org.maktab.repository.SubServiceRepository;
+
+public class SubServiceRepositoryImpl extends BaseRepositoryImpl<SubService> implements SubServiceRepository {
+}
